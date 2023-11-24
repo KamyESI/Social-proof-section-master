02cfdc60eaccf50f97ed7c35807e8d713c85f780
kamilia <k_djaid@esi.dz>
2023-11-24 15:54:24 +0100

    version 1.0
    after runing  nmp build command

--- src/Stars.js
+++ src/Stars.js
@@ -2,7 +2,7 @@ import img from "./images/icon-star.svg";
 
 function Stars(param) {
   return (
-    <div className="card bg-violet-100 flex flex-col md:flex-row justify-center items-center mx-8 md:mx-0 p-4 lg:px-8 rounded-md md:justify-between md:w-3/4">
+    <div className="card bg-violet-100 flex flex-col md:flex-row justify-center items-center mx-8 md:mx-0 p-4 lg:px-8 rounded-md md:justify-between md:w-3/4 md:self-end">
       <div className="stars-container flex gap-2 md:gap-1 mb-2 md:mb-0 ">
         <img className="md:w-3 md:h-3" src={img} alt="" />
 
@@ -14,7 +14,7 @@ function Stars(param) {
 
         <img className="md:w-3 md:h-3" src={img} alt="" />
       </div>
-      <h6 className="text-fuchsia-900 font-bold md:text-xs">{param.title}</h6>
+      <h6 className="text-fuchsia-900 font-bold md:text-xs  ">{param.title}</h6>
     </div>
   );
 }
